b73598d003701026b609c481896e90e9f584053d
Alexander Huemer <alexander.huemer@xx.vu>
2021-01-03 12:45:39 +0000

    got picture, but horizontally offset and intermittent

--- HP54542C_LCD2VGA.v
+++ HP54542C_LCD2VGA.v
@@ -25,99 +25,151 @@ module HP54542C_LCD2VGA(
 	//output wire ow_b2,
 	output wire ow_hsync,
 	output wire ow_vsync,
-	output wire D1,
-	output wire D2,
-	output wire D3,
-	output wire D4,
-	output wire D5,
+	output wire D1, // up
+	output wire D2, // right
+	output wire D3, // down
+	output wire D4, // left
+	output wire D5 // center
 );
 
-parameter p_hpixels_active = 640;
-parameter p_vga_hfp = 16;
-parameter p_vga_hsp = 96;
-parameter p_vga_hbp = 48;
-parameter p_vga_hpixels = p_hpixels_active + p_vga_hfp + p_vga_hsp + p_vga_hbp; // 800
+localparam p_hpixels_active = 640 - 1;
+localparam p_vga_hfp = 16;
+localparam p_vga_hsp = 96;
+localparam p_vga_hbp = 48;
+localparam p_vga_hpixels = p_hpixels_active + p_vga_hfp + p_vga_hsp + p_vga_hbp; // 800
 
-parameter p_vpixels_active = 480;
-parameter p_vga_vfp = 10;
-parameter p_vga_vsp = 2;
-parameter p_vga_vbp = 33;
-parameter p_vga_vpixels = p_vpixels_active + p_vga_vfp + p_vga_vsp + p_vga_vbp; // 525
+localparam p_vpixels_active = 480;
+localparam p_vga_vfp = 10;
+localparam p_vga_vsp = 2;
+localparam p_vga_vbp = 33;
+localparam p_vga_vpixels = p_vpixels_active + p_vga_vfp + p_vga_vsp + p_vga_vbp; // 525
+
+// localparam srl = 300; // shift register length
+
 
 reg r_found_start = 0;
-reg r_active_area = 0;
-reg r_hsync;
-reg r_vsync;
+//reg r_active_area = 0;
+//reg r_hsync;
+//reg r_vsync;
 reg reset = 0;
 // total number of pixels in one frame, including porches and sync: 420000
 // smallest counter large enough to support that: 2 ** 19 == 524288
-reg [18:0] r19_clk_counter = 18'b0;
+
+// xxx-verilator says this should be 32 bit wide
+// %Warning-WIDTH: HP54542C_LCD2VGA.v:94:24: Operator GTE expects 32 bits on the LHS, but LHS's VARREF 'r19_clk_counter' generates 19 bits.
+reg [31:0] r32_clk_counter = 32'b0;
 // TODO: doesn't have to be that large
-reg [18:0] r19_last_sync_pulse = 18'b0;
+reg [31:0] r32_last_sync_pulse = 32'b0;
+reg [31:0] r32_ticks_between_sync = 32'b0;
+
+reg [24:0] clock_blinker = 25'b0;
+reg clock_led = 0;
+//reg [14:0] sync_blinker = 15'b0;
+reg sync_led = 0;
+
+reg [9:0] hpos = 10'b0;
+reg [9:0] vpos = 10'b0;
+
+// reg [srl-1:0] sr_r = 300'b0;
+// reg [srl-1:0] sr_g = 300'b0;
+// reg [srl-1:0] sr_b = 300'b0;
 
 // iw_[rgb][012] should be asserted to 0 during porches anyways, but let's
 // make sure...
-assign ow_r0 = r_active_area ? iw_r0 : 0;
+//assign ow_r0 = r_active_area ? iw_r0 : 0;
+assign ow_r0 = iw_r0;
 //assign ow_r1 = r_active_area ? iw_r1 : 0;
 //assign ow_r2 = r_active_area ? iw_r2 : 0;
-assign ow_g0 = r_active_area ? iw_g0 : 0;
+//assign ow_g0 = r_active_area ? iw_g0 : 0;
+assign ow_g0 = iw_g0;
 //assign ow_g1 = r_active_area ? iw_g1 : 0;
 //assign ow_g2 = r_active_area ? iw_g2 : 0;
-assign ow_b0 = r_active_area ? iw_b0 : 0;
+//assign ow_b0 = r_active_area ? iw_b0 : 0;
+assign ow_b0 = iw_b0;
 //assign ow_b1 = r_active_area ? iw_b1 : 0;
 //assign ow_b2 = r_active_area ? iw_b2 : 0;
-assign ow_hsync = r_hsync;
-assign ow_vsync = r_vsync;
+assign ow_hsync = !((hpos > (p_hpixels_active + p_vga_hfp)) && (hpos < (p_hpixels_active + p_vga_hfp + p_vga_hsp)));
+assign ow_vsync = !((vpos > (p_vpixels_active + p_vga_vfp)) && (vpos < (p_vpixels_active + p_vga_vfp + p_vga_vsp)));
 
-assign D1 = 0;
-assign D2 = 0;
-assign D3 = 0;
+assign D1 = r_found_start ? 1 : 0;
+assign D2 = clock_led;
+assign D3 = sync_led;
 assign D4 = 0;
-assign D5 = 0;
-
-integer i;
+assign D5 = 1;
 
 always @(posedge iw_clk)
 begin
-	r19_clk_counter <= reset ? 0 : r19_clk_counter + 1;
+	r32_clk_counter <= reset ? 0 : r32_clk_counter + 1;
+
+	// sr_r <= {sr_r[srl-2:0], iw_r0};
+	// sr_g <= {sr_g[srl-2:0], iw_g0};
+	// sr_b <= {sr_b[srl-2:0], iw_b0};
+
+	if (reset)
+	begin
+		hpos <= 0;
+		vpos <= 0;
+		reset <= 0;
+	end
+	else if (hpos < p_vga_hpixels)
+		hpos <= hpos + 1'b1;
+	else
+	begin
+		hpos <= 0;
+		if (vpos < p_vga_vpixels)
+			vpos <= vpos + 1'b1;
+		else
+			vpos <= 0;
+	end
+
+	if (iw_sync)
+	begin
+		r32_ticks_between_sync <= r32_clk_counter - r32_last_sync_pulse;
+		r32_last_sync_pulse <= r32_clk_counter;
+		if (r32_ticks_between_sync > 1000)
+			reset <= 1;
+	end
 
 	// Start generating sync pulses and RGB data only after successful
 	// detection of frame boundary
 	if (r_found_start)
 	begin
-		for (i = 0; i < p_vpixels_active; i = i + 1)
-			if (r19_clk_counter >= (i * p_hpixels_active) && (r19_clk_counter < (i * p_hpixels_active + p_vga_hfp + p_vga_hsp + p_vga_hbp)))
-				r_active_area <= 1;
-			else
-				r_active_area <= 0;
-
-		for (i = 0; i < p_vga_vpixels; i = i + 1)
-
-			if (r19_clk_counter >= (i * p_hpixels_active + p_vga_hfp) && (r19_clk_counter < (i * p_hpixels_active + p_vga_hfp + p_vga_hsp)))
-				r_hsync <= 0;
-			else
-				r_hsync <= 1;
-
-		if (r19_clk_counter >= (p_vga_hpixels * (p_vpixels_active + p_vga_vfp)) && (r19_clk_counter < (p_vga_hpixels * (p_vpixels_active + p_vga_vfp + p_vga_vsp))))
-			r_vsync <= 0;
-		else
-			r_vsync <= 1;
 	end
-end
 
-always @(posedge iw_sync)
-begin
-	// 1000 is arbitrary, everything greater than 800 should work
-	// The sync signal is low during vertical front porch, sync pulse and
-	// back porch. That's 36000 clock pulses.
-	if ((r19_clk_counter - r19_last_sync_pulse) > 1000)
+	if (clock_blinker == 25000000)
 	begin
-		reset <= 1;
-		if (!r_found_start)
-			r_found_start <= 1;
+		clock_led <= clock_led ? 0 : 1;
+		clock_blinker <= 25'b0;
 	end
 	else
-		r19_last_sync_pulse = r19_clk_counter;
+		clock_blinker <= clock_blinker + 1;
 end
+
+// always @(posedge iw_sync)
+// begin
+// 	// 1000 is arbitrary, everything greater than 800 should work
+// 	// The sync signal is low during vertical front porch, sync pulse and
+// 	// back porch. That's 36000 clock pulses.
+// 	if ((r32_clk_counter - r32_last_sync_pulse) > 1000)
+// 	begin
+// 		reset <= 1;
+// 		if (!r_found_start)
+// 			r_found_start <= 1;
+// 	end
+// 	else
+// 	begin
+// 		r32_last_sync_pulse <= r32_clk_counter;
+// 		reset <= 0;
+// 	end
+// 
+// 
+// 	if (sync_blinker == 32000)
+// 	begin
+// 		sync_led <= sync_led ? 0 : 1;
+// 		sync_blinker <= 15'b0;
+// 	end
+// 	else
+// 		sync_blinker <= sync_blinker + 1;
+// end
 
 endmodule
--- Makefile
+++ Makefile
@@ -1,21 +1,26 @@
 PROJ = HP54542C_LCD2VGA
 DEVICE = 1k
 FOOTPRINT = tq144
+RES = 2048
 
 FILES = $(PROJ).v
 
 .PHONY: all clean burn
 
-all:
 	#yosys -q -p "synth_ice40 -blif $(PROJ).blif" $(FILES)
-	yosys -q -l $(PROJ)-yosys.log -p "synth_ice40 -top $(PROJ) -json $(PROJ).json" $(FILES)
 	#arachne-pnr -d $(DEVICE) -P $(FOOTPRINT) -p $(PROJ).pcf -o $(PROJ).txt $(PROJ).blif
-	nextpnr-ice40 -q -l $(PROJ)-nextpnr.log --hx1k --package $(FOOTPRINT) --json $(PROJ).json --pcf $(PROJ).pcf --asc $(PROJ).asc
 	#icepack $(PROJ).txt $(PROJ).bin
+all:
+	verilator -Wall -cc $(FILES)
+	yosys -q -l $(PROJ)-yosys.log -p "synth_ice40 -top $(PROJ) -json $(PROJ).json" $(FILES)
+	nextpnr-ice40 -q -l $(PROJ)-nextpnr.log --hx1k --package $(FOOTPRINT) --json $(PROJ).json --pcf $(PROJ).pcf --asc $(PROJ).asc --placed-svg $(PROJ)-placed.svg --routed-svg $(PROJ)-routed.svg
 	icepack $(PROJ).asc $(PROJ).bin
 
 burn:
 	iceprog $(PROJ).bin
 
+png:
+	inkscape -w $(RES) -h $(RES) $(PROJ)-placed.svg --export-filename $(PROJ)-placed.png
+	inkscape -w $(RES) -h $(RES) $(PROJ)-routed.svg --export-filename $(PROJ)-routed.png
 clean:
 	$(RM) $(PROJ).blif $(PROJ).txt $(PROJ).bin
