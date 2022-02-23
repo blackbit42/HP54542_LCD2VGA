f56beaea3261648b3ff6ee1df3a266114925d92f
Alexander Huemer <alexander.huemer@xx.vu>
2022-02-23 19:42:30 +0000

    design works!

--- rtl/HP54542C_LCD2VGA.v
+++ rtl/HP54542C_LCD2VGA.v
@@ -24,7 +24,7 @@ localparam p_vga_hsp = 96;
 localparam p_vga_hbp = 48;
 localparam p_vga_hpixels = p_hpixels_active + p_vga_hfp + p_vga_hsp + p_vga_hbp; // 800
 
-localparam p_vpixels_active = 480;
+localparam p_vpixels_active = 480 - 1;
 localparam p_vga_vfp = 10;
 localparam p_vga_vsp = 2;
 localparam p_vga_vbp = 33;
@@ -32,13 +32,9 @@ localparam p_vga_vpixels = p_vpixels_active + p_vga_vfp + p_vga_vsp + p_vga_vbp;
 
 reg r_found_start = 0;
 reg reset = 0;
-// total number of pixels in one frame, including porches and sync: 420000
-// smallest counter large enough to support that: 2 ** 19 == 524288
 
 reg [31:0] r32_clk_counter = 32'b0;
-// TODO: doesn't have to be that large
 reg [31:0] r32_last_sync_pulse = 32'b0;
-reg [31:0] r32_ticks_between_sync = 32'b0;
 
 reg [9:0] hpos = 10'b0;
 reg [9:0] vpos = 10'b0;
@@ -47,13 +43,13 @@ assign D_up = 0;
 assign D_right = 0;
 assign D_down = 0;
 assign D_left = 0;
-assign D_center = 0;
+assign D_center = r_found_start;
 
 // iw_[rgb][012] should be asserted to 0 during porches anyways, but let's
 // make sure...
-assign ow_r0 = ((hpos > p_hpixels_active) && (hpos < p_vga_hpixels)) && r_found_start ? 0 : iw_r0;
-assign ow_g0 = ((hpos > p_hpixels_active) && (hpos < p_vga_hpixels)) && r_found_start ? 0 : iw_g0;
-assign ow_b0 = ((hpos > p_hpixels_active) && (hpos < p_vga_hpixels)) && r_found_start ? 0 : iw_b0;
+assign ow_r0 = (hpos < p_hpixels_active) && (vpos < p_vpixels_active) && r_found_start ? iw_r0 : 0;
+assign ow_g0 = (hpos < p_hpixels_active) && (vpos < p_vpixels_active) && r_found_start ? iw_g0 : 0;
+assign ow_b0 = (hpos < p_hpixels_active) && (vpos < p_vpixels_active) && r_found_start ? iw_b0 : 0;
 
 assign ow_hsync = !((hpos > (p_hpixels_active + p_vga_hfp)) && (hpos < (p_hpixels_active + p_vga_hfp + p_vga_hsp)));
 assign ow_vsync = !((vpos > (p_vpixels_active + p_vga_vfp)) && (vpos < (p_vpixels_active + p_vga_vfp + p_vga_vsp)));
@@ -80,16 +76,16 @@ begin
 	end
 
 	if (iw_sync)
-	begin
-		r32_ticks_between_sync <= r32_clk_counter - r32_last_sync_pulse;
-		r32_last_sync_pulse <= r32_clk_counter;
-		if (r32_ticks_between_sync > 1000)
+		r32_last_sync_pulse <= 0;
+	else
+		r32_last_sync_pulse <= r32_last_sync_pulse + 1;
+
+		if (r32_last_sync_pulse == (p_vga_hpixels) * ((p_vga_vfp + p_vga_vsp + p_vga_vbp - 1)) + 1003) // <- magic number!
 		begin
 			reset <= 1;
 			if (!r_found_start)
 				r_found_start <= 1;
 		end
-	end
 end
 
 endmodule
